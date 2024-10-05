7432309a2018761f99c957d6e44e161001f54769
Judicaël AHYI <ludndev@gmail.com>
2024-10-05 08:43:12 +0100

    feat: create payload_dto.py

---
+++ payload_dto.py
@@ -0,0 +1,16 @@
+import json
+from dataclasses import dataclass, field, asdict
+from typing import Dict
+
+
+@dataclass
+class PayloadDto:
+    url: str
+    body: any
+    headers: Dict[str, str] = field(default_factory=lambda: {})
+
+    def to_dict(self) -> Dict:
+        return asdict(self)
+
+    def to_json(self, sort_keys: bool = False, indent: None | int | str = None) -> str:
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=sort_keys, indent=indent)
