9ca265c7d95cf8bde5b8b5e6b81391c3433bbd51
Judicaël AHYI <ludndev@gmail.com>
2024-10-05 08:56:22 +0100

    feat: set event and remove unused methods in publisher.py

--- publisher.py
+++ publisher.py
@@ -7,26 +7,7 @@ from demo.payload_dto import PayloadDto
 client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
-def publish_message(url, body, headers=None):
-    if headers is None:
-        headers = {}
-
-    # Construct the payload as a dictionary
-    payload = {
-        'url': url,
-        'body': body,
-        'headers': headers,
-    }
-
-    # Convert the payload to a JSON string
-    message = json.dumps(payload)
-
-    # Publish the message to the 'webhook' channel
-    client.publish('webhook', message)
-    print(f"Published message: {message}")
-
-
-def publish_message_from_payload_dto(payload_dto: PayloadDto):
+def publish_message(channel: str, payload_dto: PayloadDto):
     if payload_dto.headers is None:
         payload_dto.headers = {}
 
@@ -36,12 +17,14 @@ def publish_message_from_payload_dto(payload_dto: PayloadDto):
     # Convert the payload to a JSON string
     message = json.dumps(payload_dto.to_dict())
 
-    # Publish the message to the 'webhook' channel
-    client.publish('webhook', message)
-    print(f"Published message: {message}")
+    # Publish the message to the chosen channel
+    client.publish(channel, message)
+    print(f"Published on [{channel}]: {message}")
 
 
 if __name__ == '__main__':
+    event = 'payment.transaction.success'
+
     # Use the specified webhook URL
     webhook_url = 'https://webhook.site/58480b38-cea1-4c32-893a-6898ef01987c'
 
@@ -56,10 +39,10 @@ if __name__ == '__main__':
     }
 
     payload = PayloadDto(
+        event=event,
         url=webhook_url,
         body=sample_body,
         headers=headers,
     )
 
-    # publish_message(webhook_url, sample_body, headers)
-    publish_message_from_payload_dto(payload)
+    publish_message('webhook', payload)
