fd5792452f582445e4a10151cab7d9b074bf3906
Judicaël AHYI <ludndev@gmail.com>
2024-10-05 08:44:22 +0100

    feat: use payload dto in publisher.py

--- publisher.py
+++ publisher.py
@@ -1,15 +1,21 @@
 import redis
 import json
 
+from demo.payload_dto import PayloadDto
+
 # Connect to Redis
 client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
-def publish_message(url, body):
+def publish_message(url, body, headers=None):
+    if headers is None:
+        headers = {}
+
     # Construct the payload as a dictionary
     payload = {
         'url': url,
-        'body': body
+        'body': body,
+        'headers': headers,
     }
 
     # Convert the payload to a JSON string
@@ -20,14 +26,40 @@ def publish_message(url, body):
     print(f"Published message: {message}")
 
 
+def publish_message_from_payload_dto(payload_dto: PayloadDto):
+    if payload_dto.headers is None:
+        payload_dto.headers = {}
+
+    # Construct the payload as a dictionary
+    print(payload_dto.to_dict())
+
+    # Convert the payload to a JSON string
+    message = json.dumps(payload_dto.to_dict())
+
+    # Publish the message to the 'webhook' channel
+    client.publish('webhook', message)
+    print(f"Published message: {message}")
+
+
 if __name__ == '__main__':
     # Use the specified webhook URL
     webhook_url = 'https://webhook.site/58480b38-cea1-4c32-893a-6898ef01987c'
 
+    headers = {
+        'x-api-key': '<KEY>',
+    }
+
     # Sample JSON body to send
     sample_body = {
         'message': 'Hello, this is a test message!',
-        'status': 'success'
+        'status': 'success',
     }
 
-    publish_message(webhook_url, sample_body)
+    payload = PayloadDto(
+        url=webhook_url,
+        body=sample_body,
+        headers=headers,
+    )
+
+    # publish_message(webhook_url, sample_body, headers)
+    publish_message_from_payload_dto(payload)
